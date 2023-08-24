43478c5c1bf22df103ad8a75b800f2479a001aba
El-gibbor <chiagoziem11@yahoo.com>
2023-08-25 00:25:07 +0400

    fixed betty

--- select_op.c
+++ select_op.c
@@ -3,20 +3,21 @@
 /**
  * get_op_func - selects coresponding instruction to an opcode
  * @op_token: opcode instruction
-*/
+ * Return: pointer to the opcode func
+ */
 void (*get_op_func(char *op_token))(stack_t **stack, unsigned int line_number)
 {
-    instruction_t op_func[] = {
-        {"push", push},
-        {"pall", pall},
-        {NULL, NULL}
-    };
-    int i = 0;
+	instruction_t op_func[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-    for (; op_func[i].opcode; i++)
-    {
-        if (!strcmp(op_token, op_func[i].opcode))
-            return op_func[i].f;
-    }
-    return (NULL);
+	for (; op_func[i].opcode; i++)
+	{
+		if (!strcmp(op_token, op_func[i].opcode))
+			return (op_func[i].f);
+	}
+	return (NULL);
 }
