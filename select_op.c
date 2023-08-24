f57ce0f32c3b6d3cc6feda1b3297bcff56d228f9
El-gibbor <chiagoziem11@yahoo.com>
2023-08-25 00:03:07 +0400

    returns ptr to the coresponding opcode function

--- select_op.c
+++ select_op.c
@@ -1,14 +1,14 @@
 #include "monty.h"
 
 /**
- * select_opcode - selects coresponding instruction to an opcode
- * @op: opcode instruction
- * @arg: argument passed to opcode instruction
+ * get_op_func - selects coresponding instruction to an opcode
+ * @op_token: opcode instruction
 */
 void (*get_op_func(char *op_token))(stack_t **stack, unsigned int line_number)
 {
     instruction_t op_func[] = {
         {"push", push},
+        {"pall", pall},
         {NULL, NULL}
     };
     int i = 0;
@@ -18,4 +18,5 @@ void (*get_op_func(char *op_token))(stack_t **stack, unsigned int line_number)
         if (!strcmp(op_token, op_func[i].opcode))
             return op_func[i].f;
     }
+    return (NULL);
 }
