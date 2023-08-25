77e0093a7885ae0349382ef21ecfae62e89f343b
DestinedCodes <yiradesat@gmail.com>
2023-08-25 16:13:08 +0100

    Fix betty, stop print and pop top node only

--- opcodes2.c
+++ opcodes2.c
@@ -2,14 +2,12 @@
 #include <stdio.h>
 
 /**
- * swap - Swaps the top two elements of the stack.
+ * add - Adds the top two elements of the stack.
  * @stack: Pointer to the top of the stack.
  * @line_number: Current line number in the file.
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	int sum;
-
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
@@ -17,8 +15,7 @@ void add(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	sum = (*stack)->n + (*stack)->next->n;
+	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
-	printf("%d\n", sum);
 }
 
