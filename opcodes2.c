0ebf5c92545d0c075940a686f72d2622b6f4c597
El-gibbor <chiagoziem11@yahoo.com>
2023-08-25 19:02:52 +0400

    implement no opcode which does nothing

---
+++ opcodes2.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - This opcode doesn't perform any operation
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Current line number in the file.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;  /* Unused variable */
+    (void)line_number;  /* Unused variable */
+}
