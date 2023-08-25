0302ed27361816b31040219c38573339c197843d
El-gibbor <chiagoziem11@yahoo.com>
2023-08-25 19:08:10 +0400

    added nob opcode instruction to the structure

--- opcodes.c
+++ opcodes.c
@@ -83,20 +83,20 @@ void pop(stack_t **stack, unsigned int line_number)
 
 /**
  * swap - Swaps the top two elements of the stack.
- * @line_number: Current line number in the file.
  * @stack: Pointer to the top of the stack.
-*/
+ * @line_number: Current line number in the file.
+ */
 void swap(stack_t **stack, unsigned int line_number)
 {
 	int temp;
 
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		free_memory(stack);
-		exit(EXIT_FAILURE);
-	}
-	temp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = temp;
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+        free_memory(stack);
+        exit(EXIT_FAILURE);
+    }
+    temp = (*stack)->n;
+    (*stack)->n = (*stack)->next->n;
+    (*stack)->next->n = temp;
 }
