a52fd8380a6b9760351cca1f38d995fbc0e11773
DestinedCodes <yiradesat@gmail.com>
2023-08-26 08:31:12 +0100

    reduced ascii limit - mistakenly used octal limit before

--- opcodes3.c
+++ opcodes3.c
@@ -37,7 +37,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		free_memory(stack);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n > 177)
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		free_memory(stack);
